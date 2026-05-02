dd81eb65f555159a927dfe4887166127a107a3bf
El Dainosor <eldainosor@gmail.com>
2026-05-02 16:38:16 -0300

    Pongamos las publicidades detras de una flag

--- erdtvserver.py
+++ erdtvserver.py
@@ -245,9 +245,18 @@ def game_rest():
             }
 
         case 'getads':
+            # Flag para activar las falsas publicidades
+            mostrarAds=False
+
+            # Empecemos con el código para generar ads
             tipoContent = {
-                'adverts': listaAds
+                'adverts': []
             }
+
+            if mostrarAds:
+                tipoContent = {
+                    'adverts': listaAds
+                }
 
         case 'extra':
             tipoContent = {}
